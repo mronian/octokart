c43fb206fc63cd9016a8bb90a7cf0afd7897f6ce
Sabyasachee <sabyasachee.1301@gmail.com>
2016-04-14 23:16:36 +0530

    four different server for distributed system

--- octokart/logger/urls.py
+++ octokart/logger/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
                url(r'^$', views.locallogs, name='locallogs'),
+               url(r'^alllogs', views)
 #               url(r'^alllogs')
                ]
