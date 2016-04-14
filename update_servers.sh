ed472aa2576aa091ddd264fcd77c952dc4fe9eca
mronian <anchitn@gmail.com>
2016-04-14 13:45:27 +0530

    Updates script

--- update_servers.sh
+++ update_servers.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+BRANCH="origin atomictransactions"
 git add --all
 git commit -m "$1"
-git push origin master
+git push $BRANCH
 cd ~/Documents/Work/Distributed\ Systems/1000/octokart
 find . -name "*.pyc" -exec rm -rf {} \;
-git pull --no-edit
+git pull $BRANCH
 cd octokart
 if [ "$2" = "migrate" ]
 then
@@ -14,7 +15,7 @@ fi
 cd ../../..
 cd ~/Documents/Work/Distributed\ Systems/2000/octokart
 find . -name "*.pyc" -exec rm -rf {} \;
-git pull --no-edit
+git pull $BRANCH
 cd octokart
 if [ "$2" = "migrate" ]
 then
@@ -24,7 +25,7 @@ fi
 cd ../../..
 cd ~/Documents/Work/Distributed\ Systems/3000/octokart
 find . -name "*.pyc" -exec rm -rf {} \;
-git pull --no-edit
+git pull $BRANCH
 cd octokart
 if [ "$2" = "migrate" ]
 then
@@ -34,7 +35,7 @@ fi
 cd ../../..
 cd ~/Documents/Work/Distributed\ Systems/4000/octokart
 find . -name "*.pyc" -exec rm -rf {} \;
-git pull --no-edit
+git pull $BRANCH
 cd octokart
 if [ "$2" = "migrate" ]
 then
